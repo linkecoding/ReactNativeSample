4951be138c9a54077d5a4c828949c6779b843766
sidoncoding <sidoncoding@foxmail.com>
2024-08-07 01:59:05 +0800

    暂时关闭源码调试能力;新增Turbo Module测试代码

--- App.tsx
+++ App.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
-import { SafeAreaView, ScrollView, StatusBar } from 'react-native';
-import { SampleText } from './src/components/SampleText';
+import React, { useEffect } from 'react';
+import { SafeAreaView, ScrollView, StatusBar, Text } from 'react-native';
+import RTNTextHelper from 'rtn-texthelper/js/NativeRTNTextHelper';
 
 function App(): React.JSX.Element {
+  useEffect(() => {
+    const textInfo = {
+      text: '测试文本',
+      fontSize: 12,
+    };
+    const result = RTNTextHelper?.measureTextSync(textInfo);
+    console.log('===RTNTextHelper.measureTextSync==', JSON.stringify(result));
+
+    RTNTextHelper?.measureTextAsync(textInfo)
+      .then(res => {
+        console.log('===RTNTextHelper.measureTextAsync==', JSON.stringify(res));
+      })
+      .catch(() => {
+        // ignore
+      });
+  }, []);
   return (
     <SafeAreaView>
       <StatusBar barStyle={'dark-content'} />
       <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <SampleText />
+        <Text>Sample</Text>
       </ScrollView>
     </SafeAreaView>
   );
